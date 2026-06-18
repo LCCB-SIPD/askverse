0838d44578856ba962bfb7ebe1660981d353a3f9
cordystackx <mglgain@gmail.com>
2026-06-18 22:27:42 +0800

    ✨ feat(landpage): implement header component and global styles
    - Add LICENSE and SECURITY.md files.
    - Update package.json and pnpm-lock.yaml to include new dependencies.
    - Refactor globals.css to import CSS configurations.
    - Update layout.tsx metadata for the application title and description.
    - Remove page.module.css and update page.tsx to use the new Header component and 'landpage' class.
    - Create new directory structure for landpage components (header, footer).
    - Add initial implementation for the Header component.
    - Implement CSS configuration files (config.css, global_context.css).

---
+++ src/components/landpage/header/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from "./css/styles.module.css";
+
+export default function Header() {
+  return(
+    <header className={styles.container}>
+      
+    </header>
+  );
+}
