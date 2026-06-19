98ff553071a14980ea38912a69fa861b77bbe1bc
cordystackx <mglgain@gmail.com>
2026-06-19 14:51:14 +0800

    feat(header): implement landing page header component
    - add header structure with logo, navigation, and buttons
    - import and use Next.js Image component for logo
    - define CSS modules for header styling
    - include navigation links for Home, About, and FaQ
    - add a "Get Started" button
    - configure image source path in json_images/image_src.json

--- src/components/landpage/header/index.tsx
+++ src/components/landpage/header/index.tsx
@@ -1,10 +1,34 @@
 "use client";
 import styles from "./css/styles.module.css";
+import Image from "next/image";
+import img_src from "@/config/json_images/image_src.json";
 
 export default function Header() {
   return(
     <header className={styles.container}>
-      
+      <div className={styles.wrapper}>
+        <figure className={styles.logo}>
+          <Image
+            src={img_src.logo}
+            alt="Logo"
+            title="Logo"
+            width={40}
+            height={40}
+            priority
+          />
+          <figcaption>AskVerse</figcaption>
+        </figure>
+        <nav className={styles.nav_bar}>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>FaQ</li>
+          </ul>
+        </nav>
+        <div className={styles.buttons_contain}>
+          <button>Get Started</button>
+        </div>
+      </div>
     </header>
   );
 }
